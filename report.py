8d407b995db180380b7af096c33cecbd189bf835
Udgin <udgin.mail@gmail.com>
2023-08-11 17:14:30 +0200

    Changed class CDE

--- report.py
+++ report.py
@@ -10,10 +10,10 @@ cmd = argparse.ArgumentParser()
 cmd.add_argument('-d', '--dest', required=True, help='Destination')
 arg = cmd.parse_args()
 
-cde = source.CDE()
-journals = cde.getJournalPaths(cde.getHostUsers(arg.dest))
-for j in cde.getJournalData(journals):
-	print(j.name + ' --> build: ' + j.build)
+cde = source.CDE(arg.dest)
+
+for journal in cde.journals:
+	print(journal.name + ' --> build: ' + journal.build)
 
 # print(json.dumps(journals, indent = 4))
 
--- source/cde.py
+++ source/cde.py
@@ -1,47 +1,65 @@
 import os
 from .journal import RevitJournal
 
+USER_DIRECTORY = '\\C$\\Users'
+JOURNAL_DIRECTORY = '\\AppData\\Local\\Autodesk\\Revit'
+JOURNALS_FOLDER_NAME = 'Journals'
+FILTERS = {'user': ['All Users', 'archadm', 'Default', 'Default User', 'Public', 'Администратор', 'Все пользователи']}
+
 class CDE:
+	def __init__(self, host):
+		self.host = host
+		self.user_directories = None
+		self.journal_pathes = None
+		self.journals = None
 
-	def __init__(self):
-		self.filters = {}
-		self.filters['user'] = ['All Users', 'archadm', 'Default', 'Default User', 'Public', 'Администратор', 'Все пользователи']
+		self.get_host_user_directories()
+		self.get_journal_pathes()
+		self.get_journal_data()
 
 
-	def getHostUsers(self, host):
+	def get_host_user_directories(self):
+		"""Returns filtered list of the host directories for each user 
+		"""
+		host_user_directory = self.host + USER_DIRECTORY
+		user_directories = [os.path.join(host_user_directory, directory) for directory in os.listdir(host_user_directory) 
+																			if not directory in FILTERS['user']]
+		user_directories = [directory for directory in user_directories if os.path.isdir(directory)]
 
-		userPaths = []
-		path = host+'\\C$\\Users'
-		for i in os.listdir(path):
-			if os.path.isdir(os.path.join(path, i)) and not i in self.filters['user']:
-				userPaths.append(os.path.join(path, i))
-				return userPaths
+		self.user_directories = user_directories
+		#return user_directories
 
-	def getJournalPaths(self, users):
 
-		journals = []
-		for u in users:
-			path = u+'\\AppData\\Local\\Autodesk\\Revit'
-			try:
-				for i in os.listdir(path):
-					if 'Autodesk Revit 20' in i:
-						try:
-							path_j = os.path.join(path, i+'\\Journals')
-							for j in os.listdir(path_j):
-								if '.txt' in j and not '.abbrev' in j:
-									journals.append(os.path.join(path_j, j))
-									
-						except Exception as e:
-							print('no autodesk revit folders found')
-				return journals
+	def get_journal_pathes(self):
+		"""Returns list of all journal text file pathes
+		"""
+		journal_pathes = list()
+		for user_directory in self.user_directories:
+			path = user_directory + JOURNAL_DIRECTORY
 
-			except Exception as e:
-				print('no autodesk revit folders found')
+			if os.path.isdir(path):
+				revit_versions = [os.path.join(path, directory) for directory in os.listdir(path) if 'Autodesk Revit 20' in directory]
+				for revit_version in revit_versions:
+					journal_path = os.path.join(revit_version, JOURNALS_FOLDER_NAME)
 
-	def getJournalData(self, journals):
+					if os.path.isdir(journal_path):
+						current_journals = [current for current in os.listdir(journal_path) if '.txt' in current]
+						current_journals = [current for current in current_journals if '.abbrev' not in current]
+						current_journals_pathes = [os.path.join(journal_path, current) for current in current_journals]
+						journal_pathes += current_journals_pathes
+					else: print("No Autodesk Revit journal folder found:(")
 
-		result = []
-		for j in journals:
-			result.append(RevitJournal(os.path.basename(j), j))
+			else: print("No Autodesk Revit folder found:(")
 
-		return result
+		if not journal_pathes: print("No Autodesk Revit journals found:(")
+
+		self.journal_pathes = journal_pathes
+		#return journal_pathes
+
+
+	def get_journal_data(self):
+		"""Creates Journal from all text files and returns list of them
+		"""
+		journals = [RevitJournal(current) for current in self.journal_pathes]
+		self.journals = journals
+		#return journals
