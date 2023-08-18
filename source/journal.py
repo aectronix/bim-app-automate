2077a4dcdc5cafb045b45a74de139972e238bac7
Iyur <iyurrr@gmail.com>
2023-08-18 19:23:05 +0300

    Cloud and Local environments

--- source/journal.py
+++ source/journal.py
@@ -13,8 +13,8 @@ class RevitJournal:
 		self.data = {
 			'name':  os.path.basename(path),
 			# 'path': path,
-			'build': None,
-			'user': None,
+			# 'build': None,
+			# 'user': None,
 			'ops': {
 				'open': []
 			}
@@ -41,13 +41,16 @@ class RevitJournal:
 				if user:
 					self.data['user'] = user.group(1)
 
+				# todo: organize commands
+
 				# operations
 				if len(self.data['ops']['open']) > 0:
-					c = re.search(r'^\s*Jrn\.Command\s+".*(?=Ribbon|Internal|AccelKey").*', l)
-					if c:
+					c1 = re.search(r'^\s*Jrn\.Command\s+".*(?=Ribbon|Internal|AccelKey").*', l) # regular & server
+					c2 = re.search(r'Open Cloud Model Method.*', l) # cloud case
+					if c1 or c2:
 						if not 'file' in self.data['ops']['open'][-1]:
 							del self.data['ops']['open'][-1]
-						if 'ID_REVIT_FILE_OPEN' in c.group() or 'ID_APPMENU_PROJECT_OPEN' in c.group():
+						if (c1 and 'ID_REVIT_FILE_OPEN' in c1.group()) or (c1 and 'ID_APPMENU_PROJECT_OPEN' in c1.group()) or (c2 and 'Model Guid' in c2.group()):
 							self.data['ops']['open'].append({'idx': li})
 
 					# cancellation
@@ -57,16 +60,27 @@ class RevitJournal:
 							del self.data['ops']['open'][-1]
 
 				else:
-					if re.search(r'^\s*Jrn\.Command\s+".*(?=Ribbon|Internal|AccelKey")(?=.*ID_REVIT_FILE_OPEN|.*ID_APPMENU_PROJECT_OPEN).*', l):
+					if re.search(r'^\s*Jrn\.Command\s+".*(?=Ribbon|Internal|AccelKey")(?=.*ID_REVIT_FILE_OPEN|.*ID_APPMENU_PROJECT_OPEN).*', l) or re.search(r'Open Cloud Model Method.*', l):
 						self.data['ops']['open'].append({'idx': li})
-
-				# todo: ID_FILE_OPEN_CLOUD
 
 				# open
 				if len(self.data['ops']['open']) > 0:
 					cmd = self.data['ops']['open'][-1]
-					f = re.search(r'\s*"IDOK"\s*,\s*"([^"]*)"', l)
 
+					# cloud
+					file_cloud = re.search(r'Open Cloud Model Method.*Model Guid:\s*([^\s,]+)', l)
+					if file_cloud and file_cloud.group(1):
+						guid = re.search(r'"modelGuid"":""(.*?)""', lines[li-2])
+						if guid and guid.group(1) == file_cloud.group(1):
+							name = re.search(r'""displayName"":""(.*?)"",', lines[li-2])
+							if name and name.group(1):
+								print(name.group(1))
+							
+					# todo:
+					# date, host (check later Nonworkshared Cloud Local)	
+
+					# regular
+					f = re.search(r'\s*"IDOK"\s*,\s*"([^"]*)"', l)
 					if f and f.group(1):
 						dt = re.search(r'\d{2}-[A-Za-z]{3}-\d{4} \d{2}:\d{2}:\d{2}', lines[cmd['idx']-2])
 						fn = re.search(r'[\\/](?=[^\\/]*$)(.+)$', f.group(1))
