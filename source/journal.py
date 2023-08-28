36c5632625328c9cd194973a8a9486e1eac168dd
Iyur <iyurrr@gmail.com>
2023-08-28 19:33:53 +0300

    Improvements

--- source/journal.py
+++ source/journal.py
@@ -23,10 +23,10 @@ class RevitJournal:
 	def getJournalData(self):
 
 		commands = [
-			{ 'open': r'^\s*Jrn\.Command\s+".*(?=Ribbon|Internal|AccelKey")(?=.*ID_REVIT_FILE_OPEN|.*ID_APPMENU_PROJECT_OPEN).*' },		# regular
-			{ 'open': r'Open Cloud Model Method.*Model Guid:.*' },																		# cloud only
-			#{ 'save': r'save' },																										#
-			#{ 'save': r'sync' },																										#
+			{ 'open': r'^\s*Jrn\.Command\s+".*(?=Ribbon|Internal|AccelKey")(?=.*ID_REVIT_FILE_OPEN|.*ID_APPMENU_PROJECT_OPEN).*' },
+			{ 'open': r'^\s*Jrn\.RibbonEvent\s+".*ModelBrowserOpenDocumentEvent:open:.*modelGuid"".*' },
+			{ 'save': r'^\s*Jrn\.Command\s+".*(?=Ribbon|Internal|AccelKey").*Save the active project.*' },
+			#{ 'save': r'sync' },
 		]
 
 		with open(self.path, 'r') as file:
@@ -49,9 +49,12 @@ class RevitJournal:
 				# operations
 				for c in commands:
 					if re.search(c[next(iter(c))], l):
+						# q = re.search(c[next(iter(c))], l)
+						# print(q.group())
 						if len(self.data['ops']) > 0 and not 'file' in self.data['ops'][-1]:
 							del self.data['ops'][-1]
 						self.data['ops'].append({'idx': li, 'cmd': next(iter(c))})
+						# print(self.data['ops'][-1])
 						break
 
 				# cancellation
@@ -63,32 +66,32 @@ class RevitJournal:
 				if len(self.data['ops']) > 0:
 					cmd = self.data['ops'][-1] # last cmd entry
 
-					# cloud
-					file_cloud = re.search(r'Open Cloud Model Method.*Model Guid:\s*([^\s,]+)', l)
-					if file_cloud and file_cloud.group(1):
-						guid = re.search(r'"modelGuid"":""(.*?)""', lines[li-2])
-						if guid and guid.group(1) == file_cloud.group(1):
-							name = re.search(r'""displayName"":""(.*?)"",', lines[li-2])
-							date = re.search(r'\d{2}-[A-Za-z]{3}-\d{4} \d{2}:\d{2}:\d{2}', lines[cmd['idx']-1])
-							if name and name.group(1) and date:
+					if cmd['cmd'] == 'open':
+						# cloud
+						cloud_file = re.search(r'""displayName"":""(.*?)"",', l)
+						if cloud_file:
+							date = re.search(r'\d{2}-[A-Za-z]{3}-\d{4} \d{2}:\d{2}:\d{2}', lines[cmd['idx']-2])
+							if date:
 								cmd['date'] = date.group(0)
-								cmd['file'] = name.group(1)
-							
-					# regular
-					f = re.search(r'\s*"IDOK"\s*,\s*"([^"]*)"', l)
-					if f and f.group(1):
-						date = re.search(r'\d{2}-[A-Za-z]{3}-\d{4} \d{2}:\d{2}:\d{2}', lines[cmd['idx']-2])
-						fn = re.search(r'[\\/](?=[^\\/]*$)(.+)$', f.group(1))
-						if fn and fn.group(1) and date:
-							cmd['date'] = date.group(0)
-							cmd['file'] = fn.group(1)
+								cmd['file'] = cloud_file.group(1)
+								
+						# regular
+						f = re.search(r'\s*"IDOK"\s*,\s*"([^"]*)"', l)
+						if f and f.group(1):
+							date = re.search(r'\d{2}-[A-Za-z]{3}-\d{4} \d{2}:\d{2}:\d{2}', lines[cmd['idx']-2])
+							fn = re.search(r'[\\/](?=[^\\/]*$)(.+)$', f.group(1))
+							if fn and fn.group(1) and date:
+								cmd['date'] = date.group(0)
+								cmd['file'] = fn.group(1)
 
 					# worksharing state
 					if 'file' in cmd:
-						sharing = re.search(r'\s*\[Jrn\.BasicFileInfo\].*Rvt\.Attr\.Worksharing: (.*?) Rvt\.Attr\.Username:', l)
-						if sharing and sharing.group(1):
-							cmd['share'] = sharing.group(1)
-	
+						share = re.search(r'\s*\[Jrn\.BasicFileInfo\].*Rvt\.Attr\.Worksharing: (.*?) Rvt\.Attr\.Username:.*Rvt\.Attr\.LTProject:', l)
+						if share and share.group(1):
+							path = re.search(r'\s*\[Jrn\.BasicFileInfo\].*Rvt\.Attr\.Worksharing:.*Rvt\.Attr\.LastSavePath: (.*?) Rvt\.Attr\.LTProject:', l)
+							if path and path.group(1):
+								cmd['share'] = share.group(1)
+
 				li += 1
 
 
