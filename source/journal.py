fc600116b9fd1cfc1af46ce02a58586696b9039b
Iyur <iyurrr@gmail.com>
2023-08-31 19:40:38 +0300

    Completed raw logic

--- source/journal.py
+++ source/journal.py
@@ -27,7 +27,7 @@ class RevitJournal:
 			{ 'open': r'^\s*Jrn\.RibbonEvent\s+".*ModelBrowserOpenDocumentEvent:open:.*modelGuid"".*' },
 			{ 'save': r'^\s*Jrn\.Command\s+".*(?=Ribbon|Internal|AccelKey")(?=.*ID_REVIT_FILE_SAVE|.*ID_REVIT_FILE_SAVE_AS|.*ID_REVIT_SAVE_AS_TEMPLATE).*' },
 			{ 'sync': r'^\s*Jrn\.Command\s+".*(?=Ribbon|Internal|AccelKey")(?=.*ID_FILE_SAVE_TO_CENTRAL|.*ID_FILE_SAVE_TO_MASTER_SHORTCUT).*' },
-			#{ 'save': r'sync' },
+			{ 'exit': r'^\s*Jrn\.Command\s+".*(?=Ribbon|Internal|AccelKey")(?=.*ID_REVIT_FILE_CLOSE|.*ID_APP_EXIT).*' },
 		]
 
 		with open(self.path, 'r') as file:
@@ -63,9 +63,19 @@ class RevitJournal:
 					if not 'file' in self.data['ops'][-1]:
 						del self.data['ops'][-1]
 
-				# commands
+				# command cases
 				if len(self.data['ops']) > 0:
 					cmd = self.data['ops'][-1] # last cmd entry
+
+					if cmd['cmd'] == 'exit':
+						if re.search(r'\s*"TaskDialogResult".*',l):
+							rows = (lines[li], lines[li+1])
+							if any('Yes' in line for line in rows) and any('IDYES' in line for line in rows):
+								cmd['cmd'] = 'save'
+							elif any('Save to cloud' in line for line in rows) and any('1001' in line for line in rows):
+								cmd['cmd'] = 'save'
+							elif any('Synchronize with central' in line for line in rows) and any('1001' in line for line in rows):
+								cmd['cmd'] = 'sync'
 
 					if cmd['cmd'] == 'open':
 						# cloud
@@ -76,7 +86,7 @@ class RevitJournal:
 								cmd['date'] = date.group(0)
 								cmd['file'] = cloud_file.group(1)
 								
-						# regular
+						# other
 						f = re.search(r'\s*"IDOK"\s*,\s*"([^"]*)"', l)
 						if f and f.group(1):
 							date = re.search(r'\d{2}-[A-Za-z]{3}-\d{4} \d{2}:\d{2}:\d{2}', lines[cmd['idx']-2])
@@ -87,7 +97,13 @@ class RevitJournal:
 
 					if cmd['cmd'] == 'save':
 						if not 'file' in cmd:
-							# Retrieving from such lines: Server-based Central Model [identity = 61ff9ecf-148d-462a-81d9-94e7e895cd26, region = "US", path ...
+							q4 = re.search(r'\[ISL\] On save.*Adler Checksum:.*\[(.*?)\]', l)
+							if q4:
+								save_file = re.search(r'[\\/](?=[^\\/]*$)(.+)$', q4.group(1))
+								save_date = re.search(r'\d{2}-[A-Za-z]{3}-\d{4} \d{2}:\d{2}:\d{2}', lines[cmd['idx']-2])
+								if save_file and save_date:
+									cmd['date'] = save_date.group(0)
+									cmd['file'] = save_file.group(1)
 							q1 = re.search(r'Server-based Central Model \[identity.*?path = "(.*?)"]: init', l)
 							if q1:
 								sync_file = re.search(r'[\\/](?=[^\\/]*$)(.+)$', q1.group(1))
@@ -103,11 +119,14 @@ class RevitJournal:
 									cmd['date'] = sync_date.group(0)
 									cmd['file'] = sync_file.group(1)
 						else:
+							q3 = re.search(r'\s*\[Jrn\.ModelOperation\].*Rvt\.Attr\.Scenario: ModelSave.*Rvt.Attr.Worksharing: (.*?) Rvt\.Attr\.ModelState:',l)
+							if q3:
+								cmd['status'] = q3.group(1)
 							if re.search(r'\s*SLOG.*>SaveAsCentral.*', l):
 								if 'RSN' in q2.group():
-									cmd['share'] = 'RevitServer Central'
+									cmd['status'] = 'RevitServer Central'
 								else:
-									cmd['share'] = 'File-Based Central'
+									cmd['status'] = 'File-Based Central'
 
 					if cmd['cmd'] == 'sync':
 						if not 'file' in cmd:
@@ -127,27 +146,17 @@ class RevitJournal:
 								if sync_file and sync_date:
 									cmd['date'] = sync_date.group(0)
 									cmd['file'] = sync_file.group(1)
-							#
-							# q3 = re.search(, l)
-
 
 					# worksharing state
-					if 'file' in cmd and not 'share' in cmd:
+					if 'file' in cmd and not 'status' in cmd:
 						if 'ID_REVIT_FILE_SAVE_AS_CLOUD_MODEL' in lines[cmd['idx']-1]:
-							cmd['share'] = 'Nonworkshared Cloud Local'
+							cmd['status'] = 'Nonworkshared Cloud Local'
 
-						share = re.search(r'\s*\[Jrn\.BasicFileInfo\].*Rvt\.Attr\.Worksharing: (.*?) Rvt\.Attr\.Username:.*Rvt\.Attr\.LTProject:', l)
-						if share and share.group(1):
+						status = re.search(r'\s*\[Jrn\.BasicFileInfo\].*Rvt\.Attr\.Worksharing: (.*?) Rvt\.Attr\.Username:.*Rvt\.Attr\.LTProject:', l)
+						if status and status.group(1):
 							path = re.search(r'\s*\[Jrn\.BasicFileInfo\].*Rvt\.Attr\.Worksharing:.*Rvt\.Attr\.LastSavePath: (.*?) Rvt\.Attr\.LTProject:', l)
 							if path and path.group(1):
-								cmd['share'] = share.group(1)
-
-						# # save
-						# if not 'share' in cmd:
-						# 	ws = re.search(r'\[Jrn\.ModelOperation\].*?Rvt\.Attr\.Scenario: ModelSave.*?Rvt\.Attr\.Worksharing: (.*?) Rvt\.Attr\.ModelState:', l)
-						# 	if ws:
-						# 		cmd['share'] = ws.group(1)
-
+								cmd['status'] = status.group(1)
 
 				li += 1
 
