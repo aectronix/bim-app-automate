bfe09af86e0d599baafb29f64bac0f2ca6ee3764
Iyur <iyurrr@gmail.com>
2023-09-21 20:25:23 +0300

    Polishing

--- source/journal.py
+++ source/journal.py
@@ -5,58 +5,79 @@ from datetime import datetime
 
 # The bunch of schemes to extract the primary command
 commands = [
-	# { 'open': r'Jrn\.RibbonEvent.*ModelBrowserOpenDocumentEvent:open:.*modelGuid.*' },
-	# { 'open': r'Jrn\.Command.*(?=Ribbon|Internal|AccelKey")(?=.*ID_REVIT_FILE_OPEN|.*ID_APPMENU_PROJECT_OPEN|.*ID_FAMILY_OPEN|.*ID_IMPORT_IFC).*' },
-	# { 'save': r'Jrn\.Command.*(?=Ribbon|Internal|AccelKey")(?=.*ID_REVIT_FILE_SAVE|.*ID_REVIT_FILE_SAVE_AS|.*ID_REVIT_SAVE_AS_TEMPLATE|.*ID_SAVE_FAMILY|.*ID_REVIT_SAVE_AS_FAMILY).*' },
-	# { 'sync': r'Jrn\.Command.*(?=Ribbon|Internal|AccelKey")(?=.*ID_FILE_SAVE_TO_CENTRAL|.*ID_FILE_SAVE_TO_MASTER_SHORTCUT|.*ID_COLLABORATE).*' },
-	# { 'exit': r'Jrn\.Command.*(?=Ribbon|Internal|AccelKey")(?=.*ID_REVIT_FILE_CLOSE|.*ID_APP_EXIT).*' },
-	{ 'exit': r'Jrn\.Command\s+".*,\s(ID_REVIT_FILE_CLOSE|ID_APP_EXIT)"' },
-	{ 'open': r'Jrn\.Command\s+".*,\s(ID_REVIT_FILE_OPEN|ID_APPMENU_PROJECT_OPEN|ID_FAMILY_OPEN|ID_IMPORT_IFC)"' },
-	{ 'save': r'Jrn\.Command\s+".*,\s(ID_REVIT_FILE_SAVE|ID_REVIT_FILE_SAVE_AS|ID_REVIT_SAVE_AS_TEMPLATE|ID_SAVE_FAMILY|ID_REVIT_SAVE_AS_FAMILY)"' },
-	{ 'sync': r'Jrn\.Command\s+".*,\s(ID_FILE_SAVE_TO_CENTRAL|ID_FILE_SAVE_TO_MASTER_SHORTCUT|ID_COLLABORATE)"' },
+	# open from ribbon menu
+	# > Jrn.RibbonEvent "ModelBrowserOpenDocumentEvent:open:{""projectGuid"":null,""modelGuid"":null,""id"":""C:\\Users\\user\\Desktop\\saving_test.rvt"",""displayName"":""saving_test"",""region"":null,""modelVersion"":""0""}"
+	{ 'open': r'Jrn\.RibbonEvent.*(ModelBrowserOpenDocumentEvent:open)' },
+	# app commands
+	# > Jrn.Command "Ribbon"  , "Open an existing project , ID_REVIT_FILE_OPEN"
+	{ 'open': r'Jrn\.Command.*,\s(ID_REVIT_FILE_OPEN|ID_APPMENU_PROJECT_OPEN|ID_FAMILY_OPEN|ID_IMPORT_IFC)"' },
+	{ 'save': r'Jrn\.Command.*,\s(ID_REVIT_FILE_SAVE|ID_REVIT_FILE_SAVE_AS|ID_REVIT_SAVE_AS_TEMPLATE|ID_SAVE_FAMILY|ID_REVIT_SAVE_AS_FAMILY)"' },
+	{ 'sync': r'Jrn\.Command.*,\s(ID_FILE_SAVE_TO_CENTRAL|ID_FILE_SAVE_TO_MASTER_SHORTCUT|ID_COLLABORATE)"' },
+	{ 'exit': r'Jrn\.Command.*,\s(ID_REVIT_FILE_CLOSE|ID_APP_EXIT)"' },
 ]
 
 # The bunch of schemes to extract the command data
 schemes = {
 
-	# uuid
+	# Check uuid
+	# > 8c780472-dc98-48b8-869e-4255d43e8e97
 	'uuid': r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}',
 
-	# separate file from path
+	# Separate file from path
+	# > {path}
 	'file': r'[\\/](?=[^\\/]*$)(.+)$',
 
+	# Get file path and file size
+	# > Detect if missing elements for {path}: File Size: {size}, Element Count: 22691 
 	'detect': { 'pattern': r'Detect if missing elements for (.*?): File Size: (-?\d+), Element Count: \d+', 'items': ['file', 'size'] },
 	
+	# Get file name
+	# > "IDOK", "{path}"
 	'idok': { 'pattern': r'"IDOK"\s*,\s*"([^"]*)"', 'items': ['file'] },
 
+	# Get file size
+	# > Jrn.Directive "FileSizeComparison" , "20230912153851", "2021(20210426_1515(x64))", {size}
 	'fsizecomp': { 'pattern': r'FileSizeComparison.*\)".*\b(\d+)\b', 'items': ['size'] },
 	
-	'skybase_size': { 'pattern': r'skybase_model_size_bytes:.*\b(\d+)\b', 'items': ['size'] },
+	# Get file size
+	# > skybase_model_size_bytes: {size}
+	'skybase': { 'pattern': r'skybase_model_size_bytes:.*\b(\d+)\b', 'items': ['size'] },
 
-	'fileinfo': { 'pattern': r'\[Jrn\.BasicFileInfo\].*Rvt\.Attr\.Worksharing: (.*?) Rvt\.Attr\.Username:.*Rvt\.Attr\.CentralModelPath: (.*?) Rvt.Attr.RevitBuildVersion:.*Rvt\.Attr\.LastSavePath:(.*?) Rvt\.Attr\.LTProject:', 'items': ['status', 'file', 'file'] },
+	# Get worksharing status and file path
+	# > [Jrn.BasicFileInfo] Rvt.Attr.Worksharing: {status} Rvt.Attr.Username:  Rvt.Attr.CentralModelPath: {path1} Rvt.Attr.RevitBuildVersion: Autodesk Revit 2021 (Build: 20210426_1515(x64)) Rvt.Attr.LastSavePath: {path2} Rvt.Attr.LTProject: notLTProject Rvt.Attr.LocaleWhenSaved: ENU Rvt.Attr.FileExt: rvt'
+	'finfo': { 'pattern': r'\[Jrn\.BasicFileInfo\].*Rvt\.Attr\.Worksharing: (.*?) Rvt\.Attr\.Username:.*Rvt\.Attr\.CentralModelPath: (.*?) Rvt.Attr.RevitBuildVersion:.*Rvt\.Attr\.LastSavePath:(.*?) Rvt\.Attr\.LTProject:', 'items': ['status', 'file', 'file'] },
 
-	'filesizeopen': { 'pattern': r'fileSizeOnOpen:(\d+)KB', 'items': ['size'] },
+	# Get file size
+	# > C 12-Sep-2023 14:20:34.548;  fileSizeOnOpen:{size}KB
+	'fsizeopen': { 'pattern': r'fileSizeOnOpen:(\d+)KB', 'items': ['size'] },
 
+	# get worksharing status and file name
+	# > 5:< SLOG $576608a2 2023-09-11 18:51:56.272 >{status}  "{path}"
 	'saveas': { 'pattern': r'SLOG .* >(\w+)  "(.*?)"', 'items': ['status', 'file'] },
 
+	# Get worksharing status
+	# > [Jrn.ModelOperation] Rvt.Attr.Scenario: ModelSave COMMON.OS_VERSION: Microsoft Windows 10 Rvt.Attr.ModelVerEpisode: ... 3Rvt.Attr.ModelPath: RVT[...] Rvt.Attr.ModelSize: 0 Rvt.Attr.DetectDuration: 16 Rvt.Attr.Worksharing: {status}  Rvt.Attr.ModelState: Normal
 	'modelsave': { 'pattern': r'Jrn\.ModelOperation.*Rvt\.Attr\.Scenario.*ModelSave.*Rvt\.Attr\.Worksharing: (.*?) Rvt\.Attr\.ModelState:', 'items': ['status'] },
 
-	# Command process is interrupted by user pushing the ui button:
-	# >>> 'H 29-Aug-2023 23:58:27.246;   0:< Jrn.Data  _ "File Name"  , "IDCANCEL" , "" 
-	'cancel': r'IDCANCEL',
+	# Get command type
+	# > Jrn.Data "TaskDialogResult"  _ , "You have made changes to model test-srv-2_user.rvt that have not been saved.  What do you want to do?",  _ "Synchronize with central", "{code}"
+	'tsync': { 'pattern': r'You have made changes to model.*"1001"', 'items': ['type'] , 'match': 'sync' },
+	'tsave': { 'pattern': r'You have made changes to model.*"1002"', 'items': ['type'] , 'match': 'save' },
+	# > Jrn.Data  _ "TaskDialogResult" , "Do you want to save changes to collaborateTest.rvt?" ,  _ "Yes"  _ , "IDYES" 
+	# > Jrn.Data  _ "TaskDialogResult" , "This is the first time that the project has been saved since Worksharing was enabled. This project will therefore become the central model. Do you want to save this project as the central model?" ,  _ "Yes"  _ , "IDYES" 
+	'saveyes': { 'pattern': r'Do you want to save.*"IDYES"', 'items': ['type'] , 'match': 'save' },
 
-	'cancel_cloud': r'AddInJournaling.*WpfButton\(0,CancelButton\)\.Click',
+	# Detect cancellation or errors
+	# > Jrn.Data  _ "File Name"  , "IDCANCEL" , "" 
+	'idcancel': { 'pattern': r'IDCANCEL', 'items': ['stop'] , 'match': True },
+	# > Jrn.AddInEvent "AddInJournaling"  , "WpfWindow(Collaborate,Collaborate).WpfButton(0,CancelButton).Click()"
+	'btcancel': { 'pattern': r'AddInJournaling.*WpfButton\(0,CancelButton\)\.Click', 'items': ['stop'] , 'match': True },
+	# > 0:< Autodesk.Bcg.Http.HttpRequestStatusException: Forbidden: Unknown response GetModelResponse
+	'error': { 'pattern': r'HttpRequestStatusException.*Unknown response.*GetModelResponse.*', 'items': ['stop'] , 'match': True },
+	# > C 01-Sep-2023 18:07:52.112; 0:< HttpRequestFailedException "403" "Forbidden: Unknown response GetModelResponse"
+	'error403': { 'pattern': r'HttpRequestFailedException.*403.*Forbidden.*', 'items': ['stop'] , 'match': True },
 
-	# Request status is unknown (cloud cases):
-	# >>> 0:< Autodesk.Bcg.Http.HttpRequestStatusException: Forbidden: Unknown response GetModelResponse 
-	'request_unknown': r'HttpRequestStatusException.*Unknown response.*GetModelResponse.*',
-	# Requestis failed, access is forbidden (cloud case):
-	# >>> C 01-Sep-2023 18:07:12.846; 0:< HttpRequestFailedException "403" "Forbidden: Unknown response GetModelResponse" 
-	'request_failed': r'HttpRequestFailedException.*403.*Forbidden.*',
-
-	# 'detect': r'Detect if missing elements for (.*?): File Size: (\d+), Element Count: \d+',
 }
-
 
 
 class RevitJournal:
@@ -99,8 +120,8 @@ class RevitJournal:
 				for c in commands:
 					command = re.search(c[next(iter(c))], l)
 					if command:
-						# if self.data['ops'] and not 'file' in self.data['ops'][-1]:
-						# 	del self.data['ops'][-1]
+						if self.data['ops'] and not 'file' in self.data['ops'][-1]:
+							del self.data['ops'][-1]
 						date = re.search(r'\d{2}-[A-Za-z]{3}-\d{4} \d{2}:\d{2}:\d{2}', lines[li-1])
 						self.data['ops'].append({'idx': li+1, 'type': next(iter(c)), 'command': command.group(1), 'date': date.group(0)})
 						break
@@ -109,73 +130,58 @@ class RevitJournal:
 				if self.data['ops']:
 					cmd = self.data['ops'][-1]
 
-					# cancellation & del
-					if (self._get_cmd_stop([schemes['cancel']], l) and not 'file' in cmd) or self._get_cmd_stop([schemes['cancel_cloud'], schemes['request_unknown'], schemes['request_failed']], l):
+					# cancellation & errors
+					if (self._parse_by_scheme([schemes['idcancel']], l) and not 'file' in cmd) or self._parse_by_scheme([schemes['btcancel'], schemes['error'], schemes['error403']], l):
 						del self.data['ops'][-1]
 
 					# transform exit commands into the final ones
 					if cmd['type'] == 'exit':
-						if re.search(r'\s*"TaskDialogResult".*', l):
-							rows = (lines[li], lines[li+1])
-							if any('Yes' in line for line in rows) and any('IDYES' in line for line in rows):
-								cmd['cmd'] = 'save'
-							elif any('Save to cloud' in line for line in rows) and any('1001' in line for line in rows):
-								cmd['cmd'] = 'save'
-							elif any('Synchronize with central' in line for line in rows) and any('1001' in line for line in rows):
-								cmd['cmd'] = 'sync'
+						task = self._parse_by_scheme([schemes['tsync'], schemes['tsave'], schemes['saveyes']], re.sub(r'\s+', ' ', l + lines[li+1] + lines[li+2]))
+						if not task: del self.data['ops'][-1]
+						else: cmd['type'] = task['type']
 
+					# try parsing schemes to retrieve the data
 					if not 'file' in cmd:
-						data = self._parse_by_scheme([schemes['idok'], schemes['detect'], schemes['fileinfo']], l)
-						for d in data:
+						fname = self._parse_by_scheme([schemes['idok'], schemes['detect'], schemes['finfo']], l)
+						for d in fname:
 							if not d in cmd:
-								cmd[d] = data[d]
+								cmd[d] = fname[d]
 
 					if not 'size' in cmd:
-						data = self._parse_by_scheme([schemes['fsizecomp'], schemes['filesizeopen'], schemes['skybase_size'], schemes['detect']], re.sub(r'\s+', ' ', l + lines[li+1]))
-						for d in data:
+						size = self._parse_by_scheme([schemes['fsizecomp'], schemes['fsizeopen'], schemes['skybase'], schemes['detect']], re.sub(r'\s+', ' ', l + lines[li+1]))
+						for d in size:
 							if not d in cmd:
-								cmd[d] = data[d]
+								cmd[d] = size[d]
 
 					if not 'status' in cmd:
-						data = self._parse_by_scheme([schemes['fileinfo'], schemes['saveas'], schemes['modelsave']], l)
-						for d in data:
+						status = self._parse_by_scheme([schemes['finfo'], schemes['saveas'], schemes['modelsave']], l)
+						for d in status:
 							if not d in cmd:
-								cmd[d] = data[d]
+								cmd[d] = status[d]
 
 				li += 1
 
 
-		# just for case
-		# if self.data['ops'] and not 'file' in self.data['ops'][-1]:
-		# 	del self.data['ops'][-1]
-
-
-	# extrat data from specific scheme parts
 	@staticmethod
-	def _get_cmd_stop(schemes, line):
-		for s in schemes:
-			q = re.search(s, line)
-			if q:
-				return True
-				break
-
-	@staticmethod
-	def _parse_by_scheme(schemlist, line):
+	def _parse_by_scheme(schemlist, line, ):
 		result = {}
 		for s in schemlist:
 			q = re.search(s['pattern'], line)
 			if q:
-				for i in range(len(s['items'])):
-
-					if s['items'][i] == 'file':
-						result['path'] = q.group(i+1)
-						file = re.search(schemes['file'], q.group(i+1))
-						if file and not re.match(schemes['uuid'], file.group(1)):
-							result['file'] = file.group(1)
+				if not 'match' in s:
+					for i in range(len(s['items'])):
+						# distinguish name from path 
+						if s['items'][i] == 'file':
+							result['path'] = q.group(i+1)
+							file = re.search(schemes['file'], q.group(i+1))
+							if file and not re.match(schemes['uuid'], file.group(1)):
+								result['file'] = file.group(1)
+							else:
+								pass
 						else:
-							pass
+							result[s['items'][i]] = q.group(i+1)
+				elif 'match' in s:
 
-					else:
-						result[s['items'][i]] = q.group(i+1)
+					result[s['items'][0]] = s['match']
 				break
 		return result
